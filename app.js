40b3fbdf3f4fc2e7e81a04ad42f0d333a69a7ea9
52lions06 <mckelvey.william@yahoo.com>
2017-06-20 14:02:34 -0500

    Update

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("works")
-
 /* Ask yourselves "What information does this app need to know about itself?"
  The answer to that question will help you decide what needs 
  to go in your state; and remember that your state should 
@@ -14,15 +12,12 @@ console.log("works")
 // one object with properties 
 //Array of strings for choices 
 
-var mockState = {
-	views: [],questions: [{}, {}],
-}
  //--------- Section 1 --------------->
 
-
-let state = {view: ["startPage", "questionsPage", "feedbackPage", "finalPage"],
+let state = {view: "startPage",
 			  
-			   questions: [{ text: "What won the 2017 NBA championship?" , 
+			   questions: [
+                            { text: "What team won the 2017 NBA championship?" , 
 			   				 choices: ["A. Cavs", "B. Warriors", "C. Knicks", "D. Spurs"],
 			   				 answer: 1},
 			   			  
@@ -30,51 +25,153 @@ let state = {view: ["startPage", "questionsPage", "feedbackPage", "finalPage"],
 			   				 choices: ["A. 3", "B. 4", "C. 5", "D. 6"],
 			   				 answer: 2},
 
+                            { text: "Who scored the most points in NBA history?",
+			   				 choices: ["A. Michael Jordan", "B. Karl Malone", "C. Wilt Chamberlain", "D. Kareem Abdul-Jabbar"],
+			   				 answer: 3},
 			   				 ],
 
 			   	currentQuestion: 0,
 
 			   	currentScore: 0,
 
-			   	answerSelection: [{responseCorrect:"You got it right!"}, 
-			   					   {responseIncorrect: "Go back to the gym"}]
-
 }
-
-
-
-
 //------------- Modify functions Section 2 ------------------->
 //advance page function
 
-// function submitButton (){}
-function advancePage(state, view){
-	state.view = startPage;
-	console.log(advancePage);
-	console.log(state.currentScore);
-	console.log(state.currentQuestion);
-} 
 
-//results page / if (true){ "you got it right" } else {"go back to the gym"}
-//On this page, hide the other pages unless you are on the last page it will return you to the start  
-// function displayScore(){}
+var currentQuestionText = state.questions[state.currentQuestion].text
 
-// current: questions.text[0]
+function answerQuestion(event, chosenButton){
 
-//------------- Render functions Section 3 ------------------->
-let renderList = function (state, element){
-	return 
+var correctAnswer = state.questions[state.currentQuestion].choices[state.questions[state.currentQuestion].answer]
+
+
+var currentScoreIndex = state.currentScore
+var currentQuestionText = state.questions[state.currentQuestion].text
+
+
+
+console.log(currentQuestionText)
+console.log(state.questions[state.currentQuestion].choices[chosenButton])
+
+
+    if (state.questions[state.currentQuestion].choices[chosenButton] === correctAnswer) {
+    //if correct, add 1 to score
+     state.currentScore++
+        
+        console.log("correct")
+        //console.log(state.currentScore)
+        nextQuestion()
+    } else {
+        console.log("wrong")
+        console.log(state.currentScore)
+        nextQuestion()
+    }
+}
+//need checkState function
+
+function startGame(view){
+	renderList();
+    state.view = "questionsPage";
+    $('.startPage').hide()
+    $('.questionsPage').show()
+    $('.feedbackPage').hide()
+    $('.finalPage').hide()
+    renderQuestion()
 }
 
+function resetGame(view){
+    state.view = "startPage";
+    currentScore = 0
+}
+
+function nextQuestion(view, currentScore, currentQuestionText) {
+    state.currentQuestion++
+    console.log(state.currentScore)
+     for (var i = 0; i < state.questions.length; i++){
+        
+        console.log("loop working")
+            $('.startPage').hide()
+           // $('.questionsPage').hide()
+            $('.finalPage').hide()
+     }
+}
+
+//------------- Render functions Section 3 ------------------->
+
+let renderList = function(element, chosenButton){
+  //loop thru choices and put names where they should be
+  return (
+        `<div class ='answerButtons'>
+            <form action="">
+                <fieldset> 
+                    <legend>Select the correct answer</legend>
+                    <button class="btn btn1" id="0" type="submit">${state.questions[state.currentQuestion].choices[chosenButton]}</button>
+                    <button class="btn btn2" id="1" type="submit">${state.questions[state.currentQuestion].choices[chosenButton]}</button>
+                    <button class="btn btn3" id="2" type="submit">${state.questions[state.currentQuestion].choices[chosenButton]}</button>
+                    <button class="btn btn4" id="3" type="submit">${state.questions[state.currentQuestion].choices[chosenButton]}</button>
+                </fieldset>
+            </form>
+        </div>`
+            )
+    }
+ /*let renderList = function (appState, element){
+    let itemsHTML = appState.items.map(function(item, i){
+      let checkedClass =  item.checked ? 'shopping-item__checked': '';
+      return (
+        `<li data-item-index="${i}">
+          <span class="shopping-item ${checkedClass}">${item.name}
+          </span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`
+        );
+      })
+    element.html(itemsHTML);
+    };*/
+
+function renderQuestion(){
+    const curQues = state.currentQuestion
+    const ques = state.questions[curQues]
+
+    $("#questionAsk").text(ques.text);
+    $(".answerButtons .btn1").text(ques.choices[0]);
+    $(".answerButtons .btn2").text(ques.choices[1]);
+    $(".answerButtons .btn3").text(ques.choices[2]);
+    $(".answerButtons .btn4").text(ques.choices[3]);
+    console.log(ques)
+
+
+}
 
 //------------- Event listeners Section 4 ------------------->
-// function submitButton (){}
+
+$(document).ready(function() {
+
+$('.start').on('click', '', function(event){
+   
+  // 1. Change state with state mod function
+
+    startGame(state)
+
+  // 2. Invoke render function
+//   const itemIndex = $(event.currentTarget).closest('li').data('item-index');
+  });
+
+$('.btn').on('click', '', function(event, currentScore){
+    event.preventDefault();
+    var chosenButton = $(this).val()
+    answerQuestion(event, chosenButton)
+$( ".feedbackPage h2" ).html(`You have ${currentScore} correct` );
+   
+
+    });
 
 
-
-
-//------------- Initialize Section 5 ------------------->
-
-
-
-    
+})
