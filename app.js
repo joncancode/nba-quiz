aa6699577d5ebbe44a6e24da0bf4cfcc88d1b404
joncancode <jc.haines19@gmail.com>
2017-06-19 23:18:04 -0400

    adding some functions

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("works")
-
 /* Ask yourselves "What information does this app need to know about itself?"
  The answer to that question will help you decide what needs 
  to go in your state; and remember that your state should 
@@ -16,7 +14,6 @@ console.log("works")
 
  //--------- Section 1 --------------->
 
-
 let state = {view: "startPage",
 			  
 			   questions: [{ text: "What won the 2017 NBA championship?" , 
@@ -27,6 +24,9 @@ let state = {view: "startPage",
 			   				 choices: ["A. 3", "B. 4", "C. 5", "D. 6"],
 			   				 answer: 2},
 
+                            { text: "Who scored the most points in NBA history?",
+			   				 choices: ["A. Michael Jordan", "B. Karl Malone", "C. Wilt Chamberlain", "D. Kareem Abdul-Jabbar"],
+			   				 answer: 3},
 			   				 ],
 
 			   	currentQuestion: 0,
@@ -41,37 +41,54 @@ let state = {view: "startPage",
 //------------- Modify functions Section 2 ------------------->
 //advance page function
 
-function advancePage(state, view){
-	state.view = view;
+function advancePage(view){
+	state.view = view
+
+    //var currentPage = document.getElementById('startPage')
 } 
  
 
-function answerQuestion(state, questions){
-    
-    state.questions = state.questions[currentQuestion]
-    
-    if (state.questions.choice === state.question.answer) {
+function answerQuestion(event){
+
+var correctAnswer = state.questions[state.currentQuestion].choices[state.questions[state.currentQuestion].answer]
+
+console.log(state.questions[state.currentQuestion].choices)
+
+
+    if (state.questions[state.currentQuestion].choices === correctAnswer) {
     //if correct, add 1 to score
      state.currentScore++
-    console.log(state.currentScore)
+        console.log("correct")
+        console.log(state.currentScore)
+        nextQuestion()
+    } else {
+        console.log("wrong")
+        nextQuestion()
     }
 }
 
-function startGame(state, view){
+function startGame(view){
     state.view = "questionsPage";
+    
 }
 
-function resetGame(state, view){
+function resetGame(view){
     state.view = "startPage";
+}
+
+function nextQuestion(view) {
+    state.currentQuestion++
+     for (var i = 0; i < state.questions.length; i++){
+        console.log("loop")
+        state.view = "feedbackPage"
+         
+     }
 }
 
 //------------- Render functions Section 3 ------------------->
 let renderList = function(state, element){
-      return (
-        `<div class=${view}>`
-        );
-      
-    element.html(itemsHTML);
+  
+ 
     };
 
 
@@ -80,14 +97,23 @@ let renderList = function(state, element){
 $(document).ready(function() {
 
 $('.start').on('click', '', function(event){
-    console.log("starting the page")
+   
   // 1. Change state with state mod function
+
+    startGame(state)
+
   // 2. Invoke render function
-  
+//   const itemIndex = $(event.currentTarget).closest('li').data('item-index');
   });
 
-$('.answerButtons').on('submit', '', function(event){
+$('.btn').on('click', '', function(event){
     event.preventDefault();
+    answerQuestion(event)
+    //console.log($(this))
+    
+  //var chosenButton = $(".btn").val() 
+   var chosenButton = $(this)
+   console.log(chosenButton)
 
     });
 
